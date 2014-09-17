c67502267be8613d6c053131f8c435e93d55dc4c
kamimura <srikes@hotmail.co.jp>
2014-09-18 08:17:02 +0900

    ゲーム最初のカウントダウン

--- SimpleGame/src/android/simple/game/MainActivity.java
+++ SimpleGame/src/android/simple/game/MainActivity.java
@@ -1,15 +1,27 @@
 package android.simple.game;
 
-import android.os.Bundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 
+
 public class MainActivity extends Activity {
+	private int count=0;//ゲーム開始カウント用
+	private Dialog dialog;
+	private ScheduledExecutorService executor;
+	private Handler handler=new Handler();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		countdown();
 	}
 
 	@Override
@@ -18,5 +30,31 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	//カウントダウン
+	public void countdown(){
+		count=6;
+		dialog = new Dialog(this);
+		executor= Executors.newSingleThreadScheduledExecutor();
+		executor.scheduleAtFixedRate(new Runnable() {
+			public void run() {
+				count--;
+				handler.post(new Runnable(){
+					public void run(){
+						dialog.setCancelable(false);
+						if(count!=5)
+							dialog.dismiss();
+						if(count!=0)
+							dialog.setTitle(""+count);
+						else
+							dialog.setTitle("START!!");
+							dialog.show();
+					}
+				});
+				if(count==-1){
+					dialog.dismiss();
+					executor.shutdown();
+				}
+			}
+		},0,1,TimeUnit.SECONDS);
+	}
 }
