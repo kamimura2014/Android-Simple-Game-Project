06ee1b0670baffc520234987a046b0a227680b6a
kamimura <srikes@hotmail.co.jp>
2014-10-02 13:09:50 +0900

    コミット忘れ

--- SimpleGame/src/android/simple/game/MainActivity.java
+++ SimpleGame/src/android/simple/game/MainActivity.java
@@ -9,13 +9,16 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
-
+import android.view.Window;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 	private int count=0;//ゲーム開始カウント用
 	private Dialog dialog;
+	private ImageView countimage;
 	private ScheduledExecutorService executor;
 	private Handler handler=new Handler();
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +36,10 @@ public class MainActivity extends Activity {
 	//カウントダウン
 	public void countdown(){
 		count=6;
-		dialog = new Dialog(this);
+		dialog  = new Dialog(this);
+		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		countimage = new ImageView(this);
+	    dialog.setContentView(countimage);
 		executor= Executors.newSingleThreadScheduledExecutor();
 		executor.scheduleAtFixedRate(new Runnable() {
 			public void run() {
@@ -41,13 +47,33 @@ public class MainActivity extends Activity {
 				handler.post(new Runnable(){
 					public void run(){
 						dialog.setCancelable(false);
-						if(count!=5)
-							dialog.dismiss();
-						if(count!=0)
-							dialog.setTitle(""+count);
-						else
-							dialog.setTitle("START!!");
-							dialog.show();
+						switch(count){
+							case 5:
+								countimage.setImageResource(R.drawable.count_5);
+								break;
+							case 4:
+								dialog.dismiss();
+								countimage.setImageResource(R.drawable.count_4);
+								break;
+
+							case 3:
+								dialog.dismiss();
+								countimage.setImageResource(R.drawable.count_3);
+								break;
+							case 2:
+								dialog.dismiss();
+								countimage.setImageResource(R.drawable.count_2);
+								break;
+							case 1:
+								dialog.dismiss();
+								countimage.setImageResource(R.drawable.count_1);
+								break;
+							case 0:
+								dialog.dismiss();
+								countimage.setImageResource(R.drawable.count_finish);
+								break;
+						}
+						dialog.show();
 					}
 				});
 				if(count==-1){
@@ -57,4 +83,5 @@ public class MainActivity extends Activity {
 			}
 		},0,1,TimeUnit.SECONDS);
 	}
+
 }
